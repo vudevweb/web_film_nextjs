af8f501a68685c7dafe0fc1d546f8db4c4558eaf
Vu Do <vudo.contact.vn@gmail.com>
2024-05-06 23:32:33 +0700

    nưewewe

--- src/app/tim-kiem/page.jsx
+++ src/app/tim-kiem/page.jsx
@@ -1,11 +1,21 @@
-
+import Movie from "@/components/template/movie";
 
 const Page = async ({searchParams}) => {
-     console.log(searchParams);
+     const api = "https://phimapi.com/v1/api/tim-kiem?keyword=";
+     const keyword = searchParams.keyword;
+
+     const getSearch = await fetch(`${api}${keyword}&limit=12`);
+     const search = await getSearch.json();
+     const movies = search.data.items
+     console.log(movies);
 
      return (
           <div>
-               <h1>Trang tìm kiếm</h1>
+               <div className="mb-5">
+                    <h3 className="text-warning">Nội dung tìm kiếm: {keyword}</h3>
+               </div>
+
+               <Movie movies={movies} domain={search.data.APP_DOMAIN_CDN_IMAGE} />
           </div>
      );
 };
