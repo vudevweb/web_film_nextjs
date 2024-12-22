a3a3645843e89c8912d99f1402de7abc6bb97491
vudevweb <vudo.contact.vn@gmail.com>
2024-12-22 14:27:25 +0700

    Add site_name and locale to metadata for improved SEO and localization

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,8 @@ export const metadata = {
     "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-1/381271892_805928278202949_6388609339400536337_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YFb859k94L8Q7kNvgEyV2nL&_nc_ht=scontent.fsgn5-6.fna&oh=00_AfAxzB7t6FbhZPtOvgKvxggnMuv_greJ9Ge85i9sJVLKBw&oe=663DB2D2",
   url: "/",
   type: "website",
+  site_name: "Phimmoi",
+  locale: "vi_VN",
   
 };
 
